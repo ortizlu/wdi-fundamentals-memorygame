d1e16f0b7a8f099905eeae7bf4a3dc8de4f7a8f8
Luis Ortiz <ortizlu@icloud.com>
2018-08-16 15:02:08 -0400

    add functions

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,18 +1,21 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if(cardsInPlay.length === 2) {
+function checkForMatch() {
   if(cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   } else {
     alert("Sorry, try again");
   }
 }
+
+function flipCard(cardId) {
+  console.log("user flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  if(cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+};
+
+flipCard(0);
+flipCard(2);
