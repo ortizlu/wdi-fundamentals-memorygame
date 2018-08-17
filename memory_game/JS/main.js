e4511bb7153b738f86a70710364254bea8de1ddf
Luis Ortiz <ortizlu@icloud.com>
2018-08-16 22:50:52 -0400

    add reset button and score

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -18,25 +18,61 @@ var cards = [{
   suit: "diamonds",
   cardImage: "images/king-of-diamonds.png"
 }];
+
 var cardsInPlay = [];
+var score = 0;
 
-function checkForMatch() {
-  if(cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again");
-  }
-}
+//reset
+var btn = document.querySelector("button");
+btn.addEventListener('click', function() {
+  resetBoard();
+});
 
-function flipCard(cardId) {
-  console.log("user flipped " + cards[cardId].rank);
+//set score in title
+//get span tag
+var currentScore = document.querySelector('span');
+//set the score to score
+currentScore.textContent = score;
+
+function flipCard() {
+  //this refers to the cardElement inside createBoard that calls flipCard.
+  var cardId = this.getAttribute('data-id');
   cardsInPlay.push(cards[cardId].rank);
-  console.log("user flipped " + cards[cardId].suit);
-  console.log("user flipped " + cards[cardId].cardImage);
+  this.setAttribute('src', cards[cardId].cardImage);
   if(cardsInPlay.length === 2) {
     checkForMatch();
   }
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+  for(var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("data-id", i);
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+}
+
+function resetBoard() {
+  cardsInPlay = [];
+  //get cards from board
+  var cards = document.querySelectorAll('img');
+  //loop through cards
+  for(var i = 0; i < cards.length; i++) {
+    //change the image to be the back again
+    cards[i].setAttribute('src', 'images/back.png');
+  }
+}
+
+function checkForMatch() {
+  if(cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+    score++;
+    currentScore.textContent = score;
+  } else {
+    alert("Sorry, try again");
+  }
+}
+
+createBoard();
